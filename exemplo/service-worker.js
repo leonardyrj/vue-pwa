5f092d935dcc4ae6a517289ce8d3e95367750b11
Leonardy Cardoso <leonardy@sgt-carvalho.local>
2017-12-10 22:41:10 -0200

    Primeiro commit

---
+++ exemplo/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install',function(e){
+    console.log('[ServiceWorker] Installer');
+});
+
+self.addEventListener('active',function(e){
+    console.log('[ServiceWorker] Active');
+});
+
+self.addEventListener('fetch',function(e){
+    console.log('[ServiceWorker] Fetch');
+});
